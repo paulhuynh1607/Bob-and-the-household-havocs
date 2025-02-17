8fd46453876380a526b9dc2ecf982f4b92326c7f
someone <85572601+paulhuynh1607@users.noreply.github.com>
2025-02-17 18:32:45 -0400

    Update main.py

--- code/main.py
+++ code/main.py
@@ -9,32 +9,45 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Bob and The House Hold Havoc")
 clock = pygame.time.Clock()
 
-background = pygame.transform.scale(pygame.image.load("assets/background/streetOne.png").convert(), (WIDTH, HEIGHT))
+background = pygame.transform.scale(pygame.image.load(FLOOR).convert(), (WIDTH, HEIGHT))
+wall = pygame.transform.scale(pygame.image.load(WALL).convert(), (WIDTH, 100))
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.transform.rotozoom(pygame.image.load("assets/Entities/bob.png").convert_alpha(), 0, PLAYER_SIZE)
+        self.current_image = PLAYER_FRONT
         self.pos = pygame.math.Vector2(PLAYER_START_X, PLAYER_START_Y)
+        self.image = pygame.transform.rotozoom(pygame.image.load(self.current_image).convert_alpha(), 0, PLAYER_SIZE)
+        self.base_player_image = self.image
+        self.hitbox_rect = self.base_player_image.get_rect(center = self.pos)
+        self.rect = self.hitbox_rect.copy()
         self.speed = PLAYER_SPEED
+
 
     def user_input(self):
         self.velocity_x = 0
         self.velocity_y = 0
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
+            self.current_image = PLAYER_DOWN
             self.velocity_y = -self.speed
         elif keys[pygame.K_s]:
+            self.current_image = PLAYER_FRONT
             self.velocity_y = self.speed
         elif keys[pygame.K_a]:
+            self.current_image = PLAYER_LEFT
             self.velocity_x = -self.speed
         elif keys[pygame.K_d]:
+            self.current_image = PLAYER_RIGHT
             self.velocity_x = self.speed
 
     def move(self):
         self.pos += pygame.math.Vector2(self.velocity_x, self.velocity_y)
+        self.hitbox_rect.center = self.pos
+        self.rect.center = self.hitbox_rect.center
 
     def update(self):
+        self.image = pygame.transform.rotozoom(pygame.image.load(self.current_image).convert_alpha(), 0, PLAYER_SIZE)
         self.user_input()
         self.move()
 
@@ -48,8 +61,10 @@ while True:
             exit()
 
     screen.blit(background, (0, 0))
-    screen.blit(player.image, player.pos)
+    screen.blit(wall, (0, 0))
+    screen.blit(player.image, player.rect)
     player.update()
+    pygame.draw.rect(screen, "red", player.hitbox_rect, width=2)
 
     pygame.display.update()
     clock.tick(FPS)

89be67523554d4b496f84733b46a9afd1f6dd0b6
someone <85572601+paulhuynh1607@users.noreply.github.com>
2025-02-17 18:32:56 -0400

    Update setting.py

--- code/setting.py
+++ code/setting.py
@@ -1,11 +1,20 @@
 # Game Setup
-WIDTH = 1000
-HEIGHT = 1000
+WIDTH = 800
+HEIGHT = 600
 FPS = 60
 
 # Player setting
-PLAYER_START_X = 500
-PLAYER_START_Y = 500
+PLAYER_START_X = 300
+PLAYER_START_Y = 300
 
 PLAYER_SPEED = 3
 PLAYER_SIZE = 3
+
+PLAYER_FRONT = "../assets/Entities/Bob/BobFront.png"
+PLAYER_LEFT = "../assets/Entities/Bob/BobLeft.png"
+PLAYER_RIGHT = "../assets/Entities/Bob/BobRight.png"
+PLAYER_DOWN = "../assets/Entities/Bob/BobBack.png"
+
+# Background images
+FLOOR = "../assets/background/LivingRoomFloorWithCarpet.png"
+WALL = "../assets/background/WallLivingRoom.png"

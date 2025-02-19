1792f87bb28b9ecc112316d45775cf6848b37c1a
someone <85572601+paulhuynh1607@users.noreply.github.com>
2025-02-18 20:32:36 -0400

    Create Player.py

---
+++ code/Player.py
@@ -0,0 +1,55 @@
+import pygame
+from sys import exit
+import math
+from pygame.locals import *
+from setting import *
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.current_image = PLAYER_FRONT
+        self.pos = pygame.math.Vector2(PLAYER_START_X, PLAYER_START_Y)
+        self.image = pygame.transform.rotozoom(pygame.image.load(self.current_image).convert_alpha(), 0, PLAYER_SIZE)
+        self.hitbox = pygame.Rect(self.pos[0] + 45, self.pos[1] + 80, 50, 30)
+        self.hitbox_combat = (self.pos[0] + 45, self.pos[1] + 15, 50, 95)
+        self.speed = PLAYER_SPEED
+        self.velocity_x = 0
+        self.velocity_y = 0
+
+    def user_input(self, collide_list):
+        self.velocity_x = 0
+        self.velocity_y = 0
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            self.current_image = PLAYER_DOWN
+            self.velocity_y = -self.speed
+        elif keys[pygame.K_s]:
+            self.current_image = PLAYER_FRONT
+            self.velocity_y = self.speed
+        elif keys[pygame.K_a]:
+            self.current_image = PLAYER_LEFT
+            self.velocity_x = -self.speed
+        elif keys[pygame.K_d]:
+            self.current_image = PLAYER_RIGHT
+            self.velocity_x = self.speed
+
+    def move(self, collide_list):
+        # Create a temporary hitbox for the next position
+        temp_hitbox = self.hitbox.move(self.velocity_x, self.velocity_y)
+
+        # Check for collisions with the collidable objects
+        for cl in collide_list:
+            if temp_hitbox.colliderect(cl):
+                # Collision detected, do not move
+                return
+
+        # No collision, update position and hitbox
+        self.pos += pygame.math.Vector2(self.velocity_x, self.velocity_y)
+        self.hitbox.topleft = (self.pos[0] + 45, self.pos[1] + 80)
+        self.hitbox_combat = (self.pos[0] + 45, self.pos[1] + 15, 50, 95)
+
+    def update(self, collide_list):
+        self.image = pygame.transform.rotozoom(pygame.image.load(self.current_image).convert_alpha(), 0, PLAYER_SIZE)
+        self.user_input(collide_list)
+        self.move(collide_list)

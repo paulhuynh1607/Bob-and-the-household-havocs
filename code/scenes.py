2eb5781d4f2a3b1c2d13d9c9f9964f3784dd603f
someone <85572601+paulhuynh1607@users.noreply.github.com>
2025-02-18 20:31:59 -0400

    Update scenes.py

--- code/scenes.py
+++ code/scenes.py
@@ -1,4 +1,24 @@
-import pygame as pg
-import setting
-import math
+import pygame
+from setting import *
+
+class Scenes:
+    def __init__(self, screen):
+        self.current_scene = 0
+        self.screen = screen
+
+    def living_room(self):
+        background = pygame.transform.scale(pygame.image.load(FLOOR).convert(), (WIDTH, HEIGHT))
+        wall = pygame.transform.scale(pygame.image.load(WALL).convert(), (WIDTH, 100))
+        couch = pygame.transform.scale(pygame.image.load(COUCH).convert_alpha(), (138, 266))
+
+        self.screen.blit(background, (0, 0))
+        self.screen.blit(wall, (0, 0))
+        self.screen.blit(couch, (COUCH_X, COUCH_Y))
+
+
+    def change_scene(self, current_scene_num):
+        self.current_scene = current_scene_num
+        match self.current_scene:
+            case 0:
+                self.living_room()
 

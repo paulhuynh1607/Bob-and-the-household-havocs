e651c91bd9615f2af9b7bbdfdd5687f9bc6b35b5
someone <85572601+paulhuynh1607@users.noreply.github.com>
2025-02-19 18:51:41 -0400

    Update scenes.py

--- code/scenes.py
+++ code/scenes.py
@@ -14,18 +14,20 @@ class Scenes:
         self.screen.blit(background, (0, 0))
         self.screen.blit(wall, (0, 0))
         self.screen.blit(couch, (COUCH_X, COUCH_Y))
-        pygame.draw.rect(self.screen, (255,0,255), ENTER_KITCHEN_HITBOX) # I added this rect so when the player touches this they wil go the kitchen
-        # I didn't add any function to the rect, it doesn't do anything atm
 
+    def kitchen(self):
+        floor = pygame.transform.scale(pygame.image.load(K_FLOOR).convert(), (WIDTH, HEIGHT))
+        wall = pygame.transform.scale(pygame.image.load(WALL).convert(), (WIDTH, 100))
 
-    def kitchen(self): # I added this function for the kitchen
-        pass
-
-
+        self.screen.blit(floor, (0, 0))
+        self.screen.blit(wall, (0, 0))
 
     def change_scene(self, current_scene_num):
         self.current_scene = current_scene_num
         match self.current_scene:
             case 0:
                 self.living_room()
+            case 1:
+                self.kitchen()
+
 

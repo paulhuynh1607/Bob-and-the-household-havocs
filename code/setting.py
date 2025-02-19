be56d813557166a9e8517e33785b84b7fa613836
someone <85572601+paulhuynh1607@users.noreply.github.com>
2025-02-19 18:51:52 -0400

    Update setting.py

--- code/setting.py
+++ code/setting.py
@@ -16,19 +16,26 @@ PLAYER_LEFT = "../assets/Entities/Bob/BobLeft.png"
 PLAYER_RIGHT = "../assets/Entities/Bob/BobRight.png"
 PLAYER_DOWN = "../assets/Entities/Bob/BobBack.png"
 
+SIDE_RIGHT_WALL = Rect(WIDTH-10, 0, 10, HEIGHT)
+SIDE_LEFT_WALL = Rect(10, 0, 10, HEIGHT)
+DOWN_WALL = Rect(0, HEIGHT-10, WIDTH, 10)
+
+
 # Living Room Scene
 FLOOR = "../assets/background/LivingRoomFloorWithCarpet.png"
 WALL = "../assets/background/WallLivingRoomGreen.png"
 WALL_HITBOX = Rect(0, 0, WIDTH, 100)
+KITCHEN_HITBOX = Rect(WIDTH-10, 0, 10, HEIGHT)
 COUCH = "../assets/Entities/furniture/sofaGreen.png"
 COUCH_X = 150
 COUCH_Y = 200
 COUCH_HITBOX = Rect(COUCH_X, COUCH_Y, 138, 266)
-COLLIDE_LIST = [COUCH_HITBOX, WALL_HITBOX]
-# I added this part for the invisible rect (it's not invisible as of feb 18 11:01) these are the properties of the rect
-ENTER_KITCHEN_X = 798
-ENTER_KITCHEN_Y = 0
-ENTER_KITCHEN_HITBOX = Rect(ENTER_KITCHEN_X, ENTER_KITCHEN_Y, 2, HEIGHT)
-ENTER_KITCHEN_HEIGHT = 800
-ENTER_KITCHEN_WIDTH = 2
+
+COLLIDE_LIST_LIVING_ROOM = [COUCH_HITBOX, WALL_HITBOX, SIDE_LEFT_WALL, DOWN_WALL]
+
+# Kitchen Scene
+K_FLOOR = "../assets/background/KitchenFloor.png"
+LIVING_ROOM_HITBOX = Rect(10, 0, 10, HEIGHT)
+
+COLLIDE_LIST_KITCHEN = [WALL_HITBOX, SIDE_RIGHT_WALL, DOWN_WALL]
 
